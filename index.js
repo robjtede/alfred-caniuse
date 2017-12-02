ebdda15b0a78cb9f39035e279a2f3421f49cb2dd
Rob Ede <robjtede@icloud.com>
2017-12-02 19:33:54 +0000

    tranform support table format

--- index.js
+++ index.js
@@ -5,35 +5,30 @@ const browserslist = require('browserslist')
 
 const { SupportTable } = require('./src/SupportTable')
 const { filterFeatures } = require('./src/features')
-const { exactMatch, transformRes } = require('./src/utils')
+const { transformRes } = require('./src/transform')
+const { exactMatch } = require('./src/utils')
 
 const { browsersListConfig } = process.env
 
 alfy
   .fetch('https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json', {
-    // maxAge: 3600 * 12,
-    transform: body => {
-      console.log(JSON.stringify(transformRes(body)))
-      // return transformRes(body)
-    }
+    maxAge: 3600 * 12
   })
   .then(res => {
     // console.log(JSON.stringify(res))
-    // const match = exactMatch(alfy.input, res.data)
-    //
-    // if (match) {
-    //   const supportTable = new SupportTable({
-    //     featureId: match,
-    //     db: res,
-    //     browsersList: browserslist(browsersListConfig)
-    //   })
-    //
-    //   return alfy.output(supportTable.alfredItems)
-    // } else {
-    //   const featureSelectionList = filterFeatures(alfy.input, res)
-    //   return alfy.output(featureSelectionList)
-    // }
+    const match = exactMatch(alfy.input, res.data)
+
+    if (match) {
+      const supportTable = new SupportTable({
+        featureId: match,
+        db: res,
+        browsersList: browserslist(browsersListConfig)
+      })
+
+      return alfy.output(supportTable.alfredItems)
+    } else {
+      const featureSelectionList = filterFeatures(alfy.input, res)
+      return alfy.output(featureSelectionList)
+    }
   })
-  .catch(err => {
-    alfy.error(err.message)
-  })
+  .catch(alfy.error)
